41c6737f3a54c69e1e44daa5ab5f25f9bc53903f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-02 15:00:20 +0530

    objects call

--- first.js
+++ first.js
@@ -1,7 +1,17 @@
-// console.log("I'm world richest man");
-// console.log("I have a lot of money");
-// console.log("I'm having a gulfstream g800");
-// name = "Piyush";
-// console.log(name);
-age = 45;
-console.log(age);
+// // console.log("I'm world richest man");
+// // console.log("I have a lot of money");
+// // console.log("I'm having a gulfstream g800");
+// // name = "Piyush";
+// // console.log(name);
+// age = 45;
+// console.log(age);
+
+const student = {
+    fullName: "Piyush",
+    age: 20,
+    cgpa: 7.5,
+    isPass: true,
+
+    
+};
+console.log(student);
