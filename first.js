2f8b7a9248a780d10f654bfe9aed6cf4f91a16af
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-02 12:10:38 +0530

    first program

--- first.js
+++ first.js
@@ -1,3 +1,7 @@
-console.log("I'm world richest man");
-console.log("I have a lot of money");
-console.log("I'm having a gulfstream g800")
+// console.log("I'm world richest man");
+// console.log("I have a lot of money");
+// console.log("I'm having a gulfstream g800");
+// name = "Piyush";
+// console.log(name);
+age = 45;
+console.log(age);
