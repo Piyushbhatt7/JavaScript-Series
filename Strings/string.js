66a8b98029a1d6c5428ca191405cd335f387c529
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-03 22:27:13 +0530

    Strings

---
+++ Strings/string.js
@@ -0,0 +1,21 @@
+// String is a sequence of character used to represent text.
+
+// Create string
+// let str = "Hello universe";
+
+// String Length
+// str.length;
+
+// String Indices
+// str[0], str[1], str[2];
+
+// let sentence = `This is a template literal`;
+// console.log(sentence);
+
+let obj = {
+
+    item: "pen",
+    price: 10,
+};
+
+console.log(`the cost of ${obj.item} is ${obj.price} rupees`);
