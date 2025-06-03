09d60cbf7273776b7f18e2079a85ab2cad0f1622
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-03 21:53:12 +0530

    Loop practice set 2

--- Loops/Practice2.js
+++ Loops/Practice2.js
@@ -1,3 +1,7 @@
 // Create a game where you start with any random game number.
 // Ask the user to keep guessing the game number until the user enters correct value.
 
+let gameNumber = 25;
+
+let userNum = prompt("Guess the number");
+console.log(userNum)
