cabdf763248d03ba9d242c699dd5dcaf816f923a
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-03 15:06:17 +0530

    Practice set 2

---
+++ Practice2.js
@@ -0,0 +1,18 @@
+// Write a code which can give grades to students according to their scores:
+// * 80 - 100, A
+// * 70 - 89, B
+// * 60 - 69, C
+// * 50 - 59, D
+// * 0 - 49, F
+
+let grades = prompt("Enter a grades of students: ");
+
+if(grades >= 80 && grades <= 100)
+{
+    console.log("A");
+}
+
+else if(grades >= 70 && grades <= 89)
+{
+    console.log("B");
+}
