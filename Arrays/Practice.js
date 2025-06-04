2dca765fce3cbf32469a62f5ee53fda08bdaa66f
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-04 14:22:58 +0530

    arrays

--- Arrays/Practice.js
+++ Arrays/Practice.js
@@ -1,2 +1,10 @@
 // For a given array with marks of students -> [85, 97, 4, 37, 76, 60]
 // Find the average marks of the entire class.
+
+ let marks = [86, 97, 4, 37, 76, 60];
+ let sum = 0;
+
+ for(let arr=0; arr < marks.length; arr++)
+ {
+    sum += marks[arr];
+ }
