1f413223fb1f96201e00760eacd161aa7009747a
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-04 15:31:52 +0530

    updating the array value

--- Arrays/Practice.js
+++ Arrays/Practice.js
@@ -20,7 +20,7 @@ let items = [250, 645, 300, 900, 50];
 let idx = 0;
 for(let val of items)
 {
-    console.log(`value at index ${idx} = ${val}`);
+   // console.log(`value at index ${idx} = ${val}`);
     let offer = val / 10;
     items[idx] = items[idx] - offer;
     console.log(`value after offer = ${items[idx]}`)
