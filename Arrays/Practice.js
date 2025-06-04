1d85a6b8c7721efbffba147459ad59d936987e4c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-04 14:33:31 +0530

    updating the array value

--- Arrays/Practice.js
+++ Arrays/Practice.js
@@ -16,3 +16,9 @@
 // For a given array with prices of 5 items -> [250, 645, 300, 900, 50];
 // All items are offer of 10% OFF on them. Change the array to store final prices after applying the offer.
 
+let items = [250, 645, 30, 900, 50];
+
+for(let val of items)
+{
+    console.log(val)
+}
