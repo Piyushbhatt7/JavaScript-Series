389aa87c9fad7eebb2fa6c05a1bafaf07efdec5c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-04 14:29:10 +0530

    arrays

--- Arrays/Practice.js
+++ Arrays/Practice.js
@@ -1,13 +1,17 @@
 // For a given array with marks of students -> [85, 97, 4, 37, 76, 60]
 // Find the average marks of the entire class.
 
- let marks = [86, 97, 4, 37, 76, 60];
- let sum = 0;
+//  let marks = [86, 97, 4, 37, 76, 60];
+//  let sum = 0;
 
-for(let arr of marks)
-{
-    sum += arr;
-}
-let avg  =sum / marks.length;
-console.log("average of marks are: ", avg);
-console.log("sum of marks: ", sum);
+// for(let arr of marks)
+// {
+//     sum += arr;
+// }
+// let avg  = sum / marks.length;
+// console.log("average of marks are: ", avg);
+// console.log("sum of marks: ", sum);
+
+
+// For a given array with prices of 5 items -> [250, 645, 300, 900, 50];
+// All items are offer of 10% OFF on them. Change the array to store final prices after applying the offer.
