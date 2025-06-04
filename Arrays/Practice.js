fcd31a6ddf0b5ce954e81add6e7ab8a0c3e06cf4
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-04 14:38:53 +0530

    updating the array value

--- Arrays/Practice.js
+++ Arrays/Practice.js
@@ -23,6 +23,6 @@ for(let val of items)
     console.log(`value at index ${idx} = ${val}`);
     let offer = val / 10;
     items[idx] = items[idx] - offer;
-    console.log(`value after offer = ${idx}`)
+    console.log(`value after offer = ${items[idx]}`)
     idx++;
 }
