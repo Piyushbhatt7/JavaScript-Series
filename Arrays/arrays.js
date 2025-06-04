2d2412abbe6919bfa138e4a7bd88f91ab6c60bcb
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-04 12:10:26 +0530

    arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,10 @@
+// Arrays in JS
+// -> Collection of items
+
+// Create Array
+
+// let heros = ["ironman", "hulk", "batman"];
+
+// let marks = [56, 78, 90, 51, 45];
+
+// let info = ["rahul",67, true, null];
