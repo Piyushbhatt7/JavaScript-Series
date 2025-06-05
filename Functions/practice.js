884a939afb94bccedd426f9cb24e20bf55c6ca98
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-05 12:18:26 +0530

    functions in js

---
+++ Functions/practice.js
@@ -0,0 +1,19 @@
+// Functions in JS
+// Block of code that performs a specific task, can be invoked whenever needed
+
+// Function Definations
+
+// function functionName(){
+     // do some work
+// }
+
+// function functionName(para1, para2..)
+// {
+     // do some work
+// }
+
+// Function Call
+
+// functionName();
+
+
