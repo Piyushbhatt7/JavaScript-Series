a401867d60e477d7ee0ef1a69797daea660b91c1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-05 21:41:18 +0530

    functions in js

--- Functions/practice.js
+++ Functions/practice.js
@@ -41,8 +41,48 @@
 // }
 
 // sum fun
-const sum = (a,b) => {
-   console.log(a+b);
-}
+// const sum = (a,b) => {
+//    console.log(a+b);
+// }
 
-sum(2,2);
+// sum(2,2);
+
+// Q1) Practice a function using "function" keyword that takes a String as an argument & returns the number of vowels in the string.
+
+// Q2) Create an arow function to perform the same task.
+
+// function countVowels(str)
+// {
+//     let count = 0;
+//     // "HelloWorld"
+//     for(const char of str)
+//     {
+//        if(
+//         char === "a" || 
+//         char === "e" || 
+//         char === "i" || 
+//         char === "o" || 
+//         char === "u"
+//     )
+//        {
+//         count++;
+//        }
+//     }
+
+//     console.log(count);
+// }
+
+// countVowels("helloworld");
+
+// forEach Loop in Array
+
+// arr.forEach(callBackFunction)
+// CallbackFunction: Headers, it is a function to execute for each element in the Array
+
+// A callback is a function passed as an argument to another function
+
+let arr = [1,2,3,4,5];
+arr.forEach((val) => {
+
+    console.log(val);
+});
